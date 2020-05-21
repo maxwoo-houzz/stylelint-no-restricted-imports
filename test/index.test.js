d4d0533d318b98e144da4ba9142ecd2f674d7679
Max Woo <max.woo@houzz.com>
2020-05-21 16:09:38 -0700

    test support for arrays of paths/patterns

--- test/index.test.js
+++ test/index.test.js
@@ -11,12 +11,43 @@ const config = {
 	}
 };
 
+// test that arrays of paths/patterns work as well
+const configWithArrays = {
+	plugins: ['./index.js'],
+	rules: {
+		'plugin/no-restricted-imports': {
+			paths: ['test/path'],
+			patterns: ['test/pattern/a/*.css','test/**/a.css']
+		}
+	}
+};
+
 it('reports restricted imports', async () => {
 	const {
 		results: [{ warnings, parseErrors }]
 	} = await lint({
 		files: './test/has-restricted-imports.test.css',
 		config
+	});
+
+	expect(parseErrors).toHaveLength(0);
+	expect(warnings).toHaveLength(3);
+
+	const { line, column, text } = warnings[0];
+
+	expect(text).toBe(
+		'\'test/path\' import is restricted from being used by a pattern. (plugin/no-restricted-imports)'
+	);
+	expect(line).toBe(1);
+	expect(column).toBe(1);
+});
+
+it('supports arrays of restricted import paths/patterns', async () => {
+	const {
+		results: [{ warnings, parseErrors }]
+	} = await lint({
+		files: './test/has-restricted-imports.test.css',
+		config: configWithArrays
 	});
 
 	expect(parseErrors).toHaveLength(0);
